3d2afce541de3ce08c7c55b1cb29eec084e107c9
John <john.toriola.tj@gmail.com>
2025-09-08 07:57:17 +0100

    Troubleshooting

--- terraform.tf
+++ terraform.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "john-terraform-state-bucket"
     key    = "farm-stack/terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "terraform-state-locks"
+    # dynamodb_table = "terraform-state-locks"
   }
 }
