499d658af05bdba496bdfeb60251521cba4d8954
John <john.toriola.tj@gmail.com>
2025-09-14 15:31:58 +0100

    Updated codebuild image to a more recent version

--- backend_pipeline.tf
+++ backend_pipeline.tf
@@ -130,7 +130,7 @@ resource "aws_codebuild_project" "john_backend_codebuild_build" {
   }
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:7.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
 
@@ -153,7 +153,7 @@ resource "aws_codebuild_project" "john_backend_codebuild_deploy" {
   }
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:7.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
 
